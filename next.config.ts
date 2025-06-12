57372ec30bb857b1acd67f48b8bf4ceb33186dfd
Chris Randall <cdr6@illinois.edu>
2025-06-11 20:12:25 -0500

    some changes

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  // distDir: "build",
+  distDir: "build",
+  output : "export",
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
